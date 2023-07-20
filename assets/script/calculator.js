1be7befcac8a7325de8709970e6610319dca6e56
GaelTikeng <Gaelinho@yahoo.fr>
2023-07-20 10:45:18 +0100

    linters errors

--- assets/script/calculator.js
+++ assets/script/calculator.js
@@ -2,30 +2,31 @@ const display_input = document.querySelector('.input')
 const display_output = document.querySelector('.output')
 const keys = document.getElementsByClassName('key')
 const operators = document.getElementsByClassName('operator')
-let input = ""
+let input = ''
 for (let key of keys) {
   const value = key.dataset.key
   key.addEventListener('click', () => {
-    if (value == "clear") {
-      input = ""
-      display_input.innerHTML = ""
-      display_output.innerHTML = ""
-    } else if (value =="del") {
+    if (value === 'clear') {
+      input = ''
+      display_input.innerHTML = ''
+      display_output.innerHTML = ''
+    } else
+    if (value ==='del') {
       input = input.slice(0, -1)
       display_input.innerHTML = input
-    } else if (value == "pi") {
+    } else
+    if (value === 'pi') {
       let input = Math.PI
       display_input.innerHTML = input
-    }
-     else if (value == "=") {
+    } else
+    if (value === '=') {
       let result = eval(input)
       display_output.innerHTML = CleanOutput(result)
-    } else {
-      if (ValidationInput(value)) {
-        input += value
-        display_input.innerHTML = input
-      }
-    } 
+    } else
+    if (ValidationInput(value)) {
+      input += value
+      display_input.innerHTML = input
+    }
   })
 }
 
@@ -44,9 +45,9 @@ function tanhyperbolic(c) {
 
 function ValidationInput (value) {
   let last_input = input.slice(-1)
-  let operators = ["+", "-", "*", "/"]
+  let operators = ['+', '-', '*', '/']
   
-  if (value == "." && last_input ==".") {
+  if (value === '.' && last_input ==='.') {
     return false
   }
 
@@ -60,34 +61,34 @@ function ValidationInput (value) {
 
 function CleanOutput (output) {
   let output_string = output.toString()
-  let decimal = output_string.split(",")[1]
-  output_string = output_string.split(",")[0]
+  let decimal = output_string.split(',')[1]
+  output_string = output_string.split(',')[0]
 
-  let output_array = output_string.split("")
+  let output_array = output_string.split('')
   
   if (output_array.length > 3) {
-    for (let i=output_array.length - 3; i > 0; i-= 3) {
-      output_array.splice(i, 0, ",")
+    for (let i=output_array.length - 3; i > 0; i -= 3) {
+      output_array.splice(i, 0, ',')
     }
   }
 
   if (decimal) {
-    output_array.push(".")
+    output_array.push('.')
     output_array.push(decimal)
   }
 
-  return output_array.join("")
+  return output_array.join('')
 }
 
 function CleanInput (input) {
-  let input_array = input.split("")
+  let input_array = input.split('')
   let length = input_array.length
 
-  for (let i = 0; i<length; i++) {
-    if (input_array[i] == "*") {
-      input_array[i] = ` <span class="operator">x</span> `
-    } else if (input_array[i] == "/") {
-      input_array[i] = ` <span class="operator">÷</span> `
+  for (let i = 0; i < length; i ++) {
+    if (input_array[i] == '*') {
+      input_array[i] = `<span class='operator'>x</span> `
+    } else if (input_array[i] === '/') {
+      input_array[i] = ` <span class='operator'>÷</span> `
     }
   }
 }
