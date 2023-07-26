ed960efda261c9788f7089c4a2a7ac7c42a008e4
GaelTikeng <Gaelinho@yahoo.fr>
2023-07-26 09:13:48 +0100

    fixing linters errors

--- assets/script/calculator.js
+++ assets/script/calculator.js
@@ -3,7 +3,7 @@ const displayOutput = document.querySelector('.output')
 const keys = document.getElementsByClassName('key')
 const operators = document.getElementsByClassName('operator')
 let input = ''
-for (let key of keys) {
+for (const key of keys) {
   const value = key.dataset.key
   key.addEventListener('click', () => {
     if (value === 'clear') {
@@ -16,11 +16,11 @@ for (let key of keys) {
       displayInput.innerHTML = input
     } else
     if (value === 'pi') {
-      let input = Math.PI
+      const input = Math.PI
       displayInput.innerHTML = input
     } else
     if (value === '=') {
-      let result = eval(input)
+      const result = eval(input)
       displayOutput.innerHTML = CleanOutput(result)
     } else
     if (ValidationInput(value)) {
@@ -30,24 +30,23 @@ for (let key of keys) {
   })
 }
 
-function sinHyperbolic(a) {
-  let x1 = Math.sinh(a)
-  displayOutput.document.innerHTML = x1
-}
+// function sinHyperbolic(a) {
+//   let x1 = Math.sinh (a)
+//   displayOutput.document.innerHTML = x1
+// }
 
-function cosHyperbolic(b) {
-  displayOutput.document.innerHTML = Math.cosh(b)
-}
+// function cosHyperbolic (b) {
+//   displayOutput.document.innerHTML = Math.cosh(b)
+// }
 
-function tanHyperbolic(c) {
-  displayOutput.document.innerHTML = Math.tanh(c)
-}
+// function tanHyperbolic (c) {
+//   displayOutput.document.innerHTML = Math.tanh(c)
+// }
 
 function ValidationInput (value) {
-  let lastInput = input.slice(-1)
-  let operators = ['+', '-', '*', '/']
-  
-  if (value === '.' && lastInput ==='.') {
+  const lastInput = input.slice(-1)
+  const operators = ['+', '-', '*', '/']
+  if (value === '.' && lastInput === '.') {
     return false
   }
 
@@ -61,10 +60,10 @@ function ValidationInput (value) {
 
 function CleanOutput (output) {
   let outputString = output.toString()
-  let decimal = outputString.split(',')[1]
+  const decimal = outputString.split(',')[1]
   outputString = outputString.split(',')[0]
 
-  let outputArray = outputString.split('')
+  const outputArray = outputString.split('')
   // Insert a commar in the result after 3 digits
   if (outputArray.length > 3) {
     for (let i = outputArray.length - 3; i > 0; i -= 3) {
@@ -81,10 +80,8 @@ function CleanOutput (output) {
 
 // Cleanup function
 function CleanInput (input) {
-  let inputArray = input.split('')
-  let length = inputArray.length
-
-  for (let i = 0; i < length; i ++) {
+  const inputArray = input.split('')
+  for (let i = 0; i < inputArray.length; i++) {
     if (inputArray[i] === '*') {
       inputArray[i] = `<span class='operator'>x</span> `
     } else if (inputArray[i] === '/') {
